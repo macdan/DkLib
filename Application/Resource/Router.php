e11bae2dcd9dc6e714e7b9cb1cc6cf51ff5e3a85
Daniel Kendell <daniel_kendell@macdan.net>
2012-03-03 00:45:31 +0000

    added router application resource

---
+++ Application/Resource/Router.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Router.php
+ *
+ * @author Daniel Kendell <daniel.kendell@gmail.com>
+ */
+
+/**
+ * Dk_Application_Resource_Router
+ *
+ * @uses Zend_Application_Resource_Router
+ * @uses Dk_Config
+ * @package Dk
+ * @subpackage ApplicationResources
+ */
+class Dk_Application_Resource_Router extends Zend_Application_Resource_Router
+{
+	/**
+	 * Initialise Router
+	 *
+	 * @access public
+	 * @return null
+	 */
+	public function init()
+	{
+		$router = $this->getRouter();
+		$config = $this->getOptions();
+		
+		if ( isset( $config['config'] ) )
+		{
+			$config = Dk_Config::factory( $config['config'], APPLICATION_ENV );
+		}
+		else
+		{
+			$config = Dk_Config::factory( $config );
+		}
+		
+		$router->addConfig( $config );
+
+		return $router;
+	}
+}
